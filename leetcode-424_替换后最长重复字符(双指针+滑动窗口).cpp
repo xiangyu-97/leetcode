6abf3813305d3e218a3677d6866299d25f0dc89e
xiangyu-97 <84614388+xiangyu-97@users.noreply.github.com>
2021-07-06 22:14:19 +0800

    Create leetcode-424_替换后最长重复字符(双指针+滑动窗口).cpp

---
+++ leetcode-424_替换后最长重复字符(双指针+滑动窗口).cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> num(26);
+        int n = s.length();
+        int maxn = 0;
+        int left = 0, right = 0;
+        while (right < n) {
+            num[s[right] - 'A']++;
+            maxn = max(maxn, num[s[right] - 'A']);
+            if (right - left + 1 - maxn > k) {
+                num[s[left] - 'A']--;
+                left++;
+            }
+            right++;
+        }
+        return right - left;
+    }
+};
