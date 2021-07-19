c9f050971192e4a227fec7defd88681ec90a2056
xiangyu-97 <84614388+xiangyu-97@users.noreply.github.com>
2021-07-19 16:10:00 +0800

    Create 剑指offer27_翻转二叉树.cpp

---
+++ 二叉树递归操作/剑指offer27_翻转二叉树.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mirrorTree(TreeNode* root) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+        TreeNode* left = mirrorTree(root->left);
+        TreeNode* right = mirrorTree(root->right);
+        root->left = right;
+        root->right = left;
+        return root;
+    }
+};
