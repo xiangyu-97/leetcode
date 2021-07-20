ed326e00219142b59ad1be8092ac002213fc9902
xiangyu-97 <84614388+xiangyu-97@users.noreply.github.com>
2021-07-20 13:57:37 +0800

    Create 剑指offer40_最小的k个数(堆排序).cpp

---
+++ 排序/剑指offer40_最小的k个数(堆排序).cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> getLeastNumbers(vector<int>& arr, int k) {
+        vector<int> vec(k, 0);
+        if (k == 0) {
+            return vec;
+        }
+        priority_queue<int> Q;  // Q是最大堆
+        for (int i = 0; i < k; ++i) {
+            Q.push(arr[i]);
+        }
+        for (int i = k; i < (int)arr.size(); ++i) {
+            if (Q.top() > arr[i]) {
+                Q.pop();
+                Q.push(arr[i]);
+            }
+        }
+        for (int i = 0; i < k; ++i) {
+            vec[i] = Q.top();
+            Q.pop();
+        }
+        return vec;
+    }
+};
